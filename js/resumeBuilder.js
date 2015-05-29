7a515936ed7e33d185cfa0091511e593c2f97de5
Marc-Antoine Vadnais <ma_vadnais@hotmail.com>
2015-05-29 17:21:20 -0400

    Addes rest of stuff. Pretty much done.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,38 +10,45 @@ var bio = {
 	},
 	"welcomeMessage" : "Hi, Welcome to my resume", 
     "skills" : ["Awesomeness", "Web Development", "Programming", "C++", "Java", "Javascript"],
-    "biopic" : "images/me.jpg"
-};
+    "biopic" : "images/me.jpg",
+	"display" : function() {
+		var formattedName = HTMLheaderName.replace("%data%", this.name);
+		var formattedRole = HTMLheaderRole.replace("%data%", this.role);
+		$("#header").prepend(formattedRole);
+		$("#header").prepend(formattedName);
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+		var formattedBioPic = HTMLbioPic.replace("%data%", this.biopic);
+		var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", this.welcomeMessage);
+		$("#header").append(formattedBioPic);
+		$("#header").append(formattedWelcomeMessage);
 
-var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
-var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-$("#header").append(formattedBioPic);
-$("#header").append(formattedWelcomeMessage);
+		var formattedMobile = HTMLmobile.replace("%data%", this.contacts.mobile);
+		$("#topContacts").append(formattedMobile);
+		$("#footerContacts").append(formattedMobile);
+		var formattedEmail = HTMLemail.replace("%data%", this.contacts.email);
+		$("#topContacts").append(formattedEmail);
+		$("#footerContacts").append(formattedEmail);
+		var formattedTwitter = HTMLtwitter.replace("%data%", this.contacts.twitter);
+		$("#topContacts").append(formattedTwitter);
+		$("#footerContacts").append(formattedTwitter);
+		var formattedGithub = HTMLgithub.replace("%data%", this.contacts.github);
+		$("#topContacts").append(formattedGithub);
+		$("#footerContacts").append(formattedGithub);
+		var formattedLocation = HTMLlocation.replace("%data%", this.contacts.location);
+		$("#topContacts").append(formattedLocation);
+		$("#footerContacts").append(formattedLocation);
 
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-$("#topContacts").append(formattedMobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-$("#topContacts").append(formattedEmail);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-$("#topContacts").append(formattedTwitter);
-var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-$("#topContacts").append(formattedGithub);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-$("#topContacts").append(formattedLocation);
-
-if (bio.skills.length > 0) {
-	$("#header").append(HTMLskillsStart);
-	var formattedSkills;
-	for (var i = 0; i < bio.skills.length; i++) {
-		formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
-		$("#skills").append(formattedSkills);
+		if (this.skills.length > 0) {
+			$("#header").append(HTMLskillsStart);
+			var formattedSkills;
+			for (var i = 0; i < this.skills.length; i++) {
+				formattedSkills = HTMLskills.replace("%data%", this.skills[i]);
+				$("#skills").append(formattedSkills);
+			}
+		}
 	}
-}
+};
+bio.display();
 
 var work = {
 	"jobs" : [
@@ -82,10 +89,7 @@ var work = {
 		}
 	}
 };
-
 work.display();
-
-
 
 var education = {
 	"schools" : [
@@ -96,6 +100,22 @@ var education = {
 			"majors" : ["Foundations of Computing"],
 			"dates" : 2005,
 			"url" : "https://www.mcgill.ca/"
+		},
+		{
+			"name" : "The Arts Institute at Bournemouth",
+			"location" : "Bournemouth",
+			"degree" : "Bachelor of Arts",
+			"majors" : ["Film and Animation"],
+			"dates" : 2002,
+			"url" : "http://aub.ac.uk/"
+		},
+		{
+			"name" : "The Arts Institute at Bournemouth",
+			"location" : "Bournemouth",
+			"degree" : "BTEC",
+			"majors" : ["Foundation in Art and Design"],
+			"dates" : 1999,
+			"url" : "http://aub.ac.uk/"
 		}
 	],
 	"onlineCourses" : [
@@ -105,8 +125,40 @@ var education = {
 			"date" : 2015,
 			"url" : "https://www.udacity.com/"
 		}
-	]
+	],
+	"display" : function() {
+		for (var school in this.schools) {
+			$("#education").append(HTMLschoolStart);
+			var formattedName = HTMLschoolName.replace("%data%", this.schools[school].name);
+			var formattedDegree = HTMLschoolDegree.replace("%data%", this.schools[school].degree);
+			$(".education-entry:last").append(formattedName + formattedDegree);
+			var formattedDates = HTMLschoolDates.replace("%data%", this.schools[school].dates);
+			$(".education-entry:last").append(formattedDates);
+			var formattedLocation = HTMLschoolLocation.replace("%data%", this.schools[school].location);
+			$(".education-entry:last").append(formattedLocation);
+			for (var major in this.schools[school].majors) {
+				var formattedMajor = HTMLschoolMajor.replace("%data%", this.schools[school].majors[major]);
+				$(".education-entry:last").append(formattedMajor);
+			}
+			var formattedURL = HTMLonlineURL.replace("%data%", this.schools[school].url);
+			$(".education-entry:last").append(formattedURL);
+		}
+		if (this.onlineCourses.length > 0) {
+			$("#education").append(HTMLonlineClasses);
+			for (var onlineCourse in this.onlineCourses) {
+				$("#education").append(HTMLschoolStart);
+				var formattedTitle = HTMLonlineTitle.replace("%data%", this.onlineCourses[onlineCourse].title);
+				var formattedSchool = HTMLonlineSchool.replace("%data%", this.onlineCourses[onlineCourse].school);
+				$(".education-entry:last").append(formattedTitle + formattedSchool);
+				var formattedDates = HTMLonlineDates.replace("%data%", this.onlineCourses[onlineCourse].date);
+				$(".education-entry:last").append(formattedDates);
+				var formattedURL = HTMLonlineURL.replace("%data%", this.onlineCourses[onlineCourse].url);
+				$(".education-entry:last").append(formattedURL);
+			}
+		}
+	}
 };
+education.display();
 
 var projects = {
 	"projects" : [
@@ -122,10 +174,37 @@ var projects = {
 				"http://www.feedbaccstudios.com/race_3_wide.jpg"
 			]
 		}
-	]
+	],
+	"display" : function() {
+		for(var project in this.projects) {
+			$("#projects").append(HTMLprojectStart);
+			var formattedTitle = HTMLprojectTitle.replace("%data%", this.projects[project].title);
+			$(".project-entry:last").append(formattedTitle);
+			var formattedDates = HTMLprojectDates.replace("%data%", this.projects[project].dates);
+			$(".project-entry:last").append(formattedDates);
+			var formattedDescription = HTMLprojectDescription.replace("%data%", this.projects[project].description);
+			$(".project-entry:last").append(formattedDescription);
+			for(var image in this.projects[project].images){
+				var formattedImage = HTMLprojectImage.replace("%data%", this.projects[project].images[image]);
+				$(".project-entry:last").append(formattedImage);
+			}
+		}
+	}
 };
+projects.display();
 
-$(document).click(function(loc) {
-  logClicks(loc.pageX, loc.pageY);
-});
+function inName(oldName) {
+    var finalName = oldName;
+    var nameArray = finalName.split(' ');
+    var lowerFirstName = nameArray[0].toLowerCase();
+    var upperLastName = nameArray[1].toUpperCase();
+    var firstName = lowerFirstName.charAt(0).toUpperCase() + lowerFirstName.substring(1);
+    finalName = firstName + ' ' + upperLastName;
+    return finalName;
+}
+
+$("#main").append(internationalizeButton);
+
+$("#mapDiv").append(googleMap);
+
 
